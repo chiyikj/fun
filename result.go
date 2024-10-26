30d45016b843103ad6dcafae9d970e5309cee023
chiyikj <cyi.cc@outlook.com>
2024-10-26 18:03:27 +0800

    完善推送服务器断开通知

--- result.go
+++ result.go
@@ -1,27 +1,28 @@
 package fun
 
 const (
-	success = iota
-	cellError
-	returnError
+	Success = iota
+	CellError
+	ReturnError
+	CloseError
 )
 
 type Result struct {
 	Id   string
-	Code *uint16
+	Code uint16
 	//通用的类型修饰 在error的情况下表示异常
 	Data   any
-	status uint8
+	Status uint8
 }
 
 func resultSuccess(data any) Result {
-	return Result{Data: data, status: success}
+	return Result{Data: data, Status: Success}
 }
 
 func Error(code uint16, msg string) Result {
-	return Result{Code: &code, Data: msg, status: returnError}
+	return Result{Code: code, Data: msg, Status: ReturnError}
 }
 
 func resultCallError(msg any) Result {
-	return Result{Data: msg, status: cellError}
+	return Result{Data: msg, Status: CellError}
 }
