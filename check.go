b3d11976601900a40418701ce7cdd66a1302e4b4
chiyi <cyi.cc@outlook.com>
2025-08-20 23:04:12 +0800

    禁止泛型

--- check.go
+++ check.go
@@ -145,7 +145,7 @@ func checkType(t reflect.Type) {
 	if t.Kind() == reflect.Ptr {
 		t = t.Elem()
 	}
-	if strings.Contains(t.String(), "[") {
+	if !strings.Contains(t.String(), "[]") && strings.Contains(t.String(), "[") {
 		panic("Fun: " + t.Name() + " generic types are not supported")
 	}
 	switch t.Kind() {
